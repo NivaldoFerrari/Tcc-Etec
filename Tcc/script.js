a45a932ecb315f737eea5108746054d48b8d0bf9
Nivaldo Ferrari Junior <nivaldoferrari79@gmail.com>
2024-06-15 22:44:57 -0300

    Adicionando oque foi feito até agora

---
+++ Tcc/script.js
@@ -0,0 +1,9 @@
+// Animação do menu Bara de navegação
+const menu = document.querySelector(".menu")
+const nav = document.querySelector(".nav")
+
+menu.addEventListener("click",()=>{
+    nav.classList.toggle("active")
+})
+
+
